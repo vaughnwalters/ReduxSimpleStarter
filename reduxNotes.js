d2179daa19913619db448c5b4b12c6d9cc67647f
Vaughn Walters <vaughnwalters@gmail.com>
2016-12-01 16:02:10 -0600

    finishing up youtube clone

---
+++ reduxNotes.js
@@ -0,0 +1,10 @@
+redux is a predictable state container for javascript containers
+- a colelction of all the data that describes the app
+  this includes not only the hard data, but also includes, for example a curretly selsected piece of data
+  - react contains the views
+
+all of the data is inside a single object, called the state.  there is a distinction between component level state, and application level state... which is redux
+
+redux contains the state of the application, that tells the components what they should render
+
+
